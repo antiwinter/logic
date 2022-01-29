370516a482098c7a7d16955378fb37b2bbec3008
antiwinter <antiwinter@126.com>
2022-01-29 11:50:57 +0800

    remove comments before relint

--- relint.js
+++ relint.js
@@ -1,16 +1,14 @@
 const { fonts, renderPixels } = require('js-pixel-fonts')
 const log = console.log
 
-function render(s, pt) {
-
+function render (s, pt) {
   // log('pt is', pt)
   let _pt
 
   log('pt is', pt, pt.split('\\n'))
   pt.split('\\n').forEach(p => {
     p = renderPixels(p, fonts.sevenPlus)
-    if (!_pt)
-      _pt = p
+    if (!_pt) _pt = p
     else {
       _pt.push(_pt[0].map(x => 0))
       _pt = _pt.concat(p)
@@ -19,32 +17,30 @@ function render(s, pt) {
   pt = _pt
 
   let area = 0
-  s.forEach(x => area += x.length)
+  s.forEach(x => (area += x.length))
 
   let n = 0
-  pt.forEach(a => a.forEach(x => n += x))
+  pt.forEach(a => a.forEach(x => (n += x)))
 
   let scaler = Math.sqrt(area / n)
   log('area: ', area, 'pix: ', n, 'scaler: ', Math.sqrt(area / n))
 
-  function empty(x, y) {
+  function empty (x, y) {
     y = Math.floor(y / scaler)
     x = Math.floor(x / scaler)
 
     return !pt[y][x]
   }
 
-  function getArea(x, y) {
+  function getArea (x, y) {
     let res = { l: 0, r: 0 }
     for (let i = x - 1; i >= 0; i--) {
-      if (empty(i, y))
-        break
+      if (empty(i, y)) break
       res.l++
     }
 
     for (let i = x; i < pt[0].length * scaler; i++) {
-      if (empty(i, y))
-        break
+      if (empty(i, y)) break
       res.r++
     }
 
@@ -66,8 +62,7 @@ function render(s, pt) {
       if (pick && (!a.l || a.r > Math.abs(pick.length - a.r))) {
         _s += pick
         x += pick.length - 1
-      } else if (pick)
-        s.unshift(pick)
+      } else if (pick) s.unshift(pick)
     }
 
     _s += '\n'
@@ -77,20 +72,28 @@ function render(s, pt) {
   return _s
 }
 
-function seg(src, pt) {
-  let def = [], code = []
-  src.replace(/\r/g, '\n').split('\n').forEach(l => {
-    l = l.trim()
-    if (l.match(/^`define|^`ifdef|^`else|^`endif/)) def.push(l)
-    else code.push(l)
-  })
+function seg (src, pt) {
+  let def = [],
+    code = []
+  src
+    .replace(new RegExp('//.+', 'g'), '\n')
+    .replace(/\r/g, '\n')
+    .split('\n')
+    .forEach(l => {
+      l = l.trim()
+      if (l.match(/^`define|^`ifdef|^`else|^`endif/)) def.push(l)
+      else code.push(l)
+    })
 
   code = code.join('\n')
 
   let eat = ' []!@#$%^*()-+;:/,.\n'.split('')
 
   eat.forEach(e => {
-    code = code.split(e).map(x => x.trim()).join('\r' + (e == '\n' ? ' ' : e) + '\r')
+    code = code
+      .split(e)
+      .map(x => x.trim())
+      .join('\r' + (e == '\n' ? ' ' : e) + '\r')
   })
 
   let compact = []
@@ -100,7 +103,6 @@ function seg(src, pt) {
 
   return def.join('\n') + '\n\n' + render(compact, pt)
 }
-
 
 module.exports = (src, pt) => {
   return seg(src, pt)
